31f13d222256600c281dab1febd40ec0f179c113
DGV1995 <diegogarciaviana.1995@gmail.com>
2021-07-23 13:12:07 +0200

    Add Item table to the database

---
+++ InAndOut/InAndOut/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using InAndOut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InAndOut.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// Item table
+        /// </summary>
+        public DbSet<Item> Item { get; set; }
+    }
+}
